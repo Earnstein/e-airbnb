ba0470f1f45c96cd2782525f757ff19bafbfbfb8
Bakare Damilola <bdamilola00@gmail.com>
2024-11-08 01:55:00 +0100

    feat: initialised a nest lib  "common "

---
+++ libs/common/src/database/database.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '../config/config.module';
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+      imports: [ConfigModule],
+    }),
+  ],
+})
+export class DatabaseModule {}
