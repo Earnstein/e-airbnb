50036789b6be97857526448e296cabe363c57c2a
Bakare Damilola <bdamilola00@gmail.com>
2024-11-08 09:19:51 +0100

    feat: added static method for model

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,16 +1,20 @@
-import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '../config/config.module';
+import { Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { ModelDefinition, MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule } from "../config/config.module";
 @Module({
   imports: [
     MongooseModule.forRootAsync({
       useFactory: (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGODB_URI'),
+        uri: configService.get<string>("MONGODB_URI"),
       }),
       inject: [ConfigService],
       imports: [ConfigModule],
     }),
   ],
 })
-export class DatabaseModule {}
+export class DatabaseModule {
+  static forFeature(models: ModelDefinition[]) {
+    return MongooseModule.forFeature(models);
+  }
+}
